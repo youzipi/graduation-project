0bf6adfce7d65c58436279e9bf281453f0072a2c
youzipi <blake_12@qq.com>
2016-04-15 15:58:12 +0800

    test(pandas): pandas-exercise,中日年号统计比较

---
+++ notebook/jp-cn-nianhao-comparison.py
@@ -0,0 +1,168 @@
+
+# coding: utf-8
+
+# wikipedia:
+# 
+# https://zh.wikipedia.org/wiki/日本年號列表
+# 
+# https://zh.wikipedia.org/wiki/中国年号列表
+# 
+# ```js
+# a = $('#mw-content-text > table> tbody > tr > td:nth-child(2) > a');
+# 
+# a = $('#mw-content-text > table> tbody > tr > td:nth-child(1) > a');
+# 
+# for (i in a){console.log(a[i].innerText)};
+# ```
+# 浏览器控制台，save as... [xxx.log]
+
+# In[69]:
+
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[251]:
+
+def render(path):
+    with open(path,'r') as file:
+        datas = file.readlines()
+    str_list = []
+    length = len(datas)
+    for i in xrange(length):
+        b = unicode(datas[i].replace('\n',''), "utf-8")
+        for j in b:
+            str_list += j
+    df = pd.DataFrame(data=str_list,columns=['nianhao'])
+    return df
+
+
+# In[252]:
+
+jp_df = render('./japan-nianhao.log')
+
+
+# In[199]:
+
+a = unicode(jp_datas[0],"utf-8")
+
+
+# In[200]:
+
+a
+print a
+
+
+# In[201]:
+
+for i in a:
+    print i
+
+
+# In[103]:
+
+a = u'中国'
+
+
+# In[106]:
+
+a
+
+
+# In[107]:
+
+type(a)
+
+
+# In[102]:
+
+print a
+
+
+# In[307]:
+
+jp_df[u'nianhao'].value_counts()
+
+
+# In[243]:
+
+jp_counts = len(jp_df[u'nianhao'].values) # 一共使用了506个字次
+jp_counts
+
+
+# In[245]:
+
+jp_words = len(np.unique(jp_df[u'nianhao'].values)) # 一共使用了72个不同的字
+jp_words
+
+
+# In[246]:
+
+jp_counts/float(jp_words)
+
+
+# In[253]:
+
+cn_df = render('./china-nianhao.log')
+
+
+# In[259]:
+
+cn_df[u'nianhao'].value_counts()[:10]
+
+
+# In[255]:
+
+cn_counts = len(cn_df[u'nianhao'].values) 
+cn_counts
+
+
+# In[258]:
+
+cn_words = len(np.unique(cn_df[u'nianhao'].values)) 
+cn_words
+
+
+# In[270]:
+
+cn_counts/float(cn_words)
+
+
+# In[276]:
+
+cn_unique_words = pd.DataFrame(data=np.unique(cn_df[u'nianhao'].values),columns=['nianhao'])
+
+
+# In[277]:
+
+jp_unique_words = pd.DataFrame(data=np.unique(jp_df[u'nianhao'].values),columns=['nianhao'])
+
+
+# In[291]:
+
+jp_index = pd.Index(jp_unique_words)
+print jp_index
+cn_index = pd.Index(cn_unique_words)
+print cn_index
+
+
+# In[302]:
+
+both = cn_index.intersection(jp_index)
+print both
+print both.size
+
+
+# In[305]:
+
+jp_only = jp_index.difference(cn_index)
+print jp_only
+print jp_only.size
+
+
+# In[ ]:
+
+
+
