59acaf58f4d2f87fbeb3152bb55e801491a2948c
youzipi <blake_12@qq.com>
2016-04-21 14:23:09 +0800

    docs(mongo): shell

---
+++ esi/esi/test/mongo_shell/field_trim.js
@@ -0,0 +1,12 @@
+db.test.find({}, {"research_areas": 1}).forEach(function (doc) {
+    var cleaned = [];
+    doc.research_areas.forEach(function (s) {
+        cleaned.push(s.trim())
+    });
+    print(cleaned);
+    db.test.update(
+        {"_id": doc._id},
+        {"$set": {"research_areas": cleaned}}
+    );
+});
+
