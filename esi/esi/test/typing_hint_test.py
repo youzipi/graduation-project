c0f46a405c5df731736e790190c642439beafa56
youzipi <blake_12@qq.com>
2016-04-21 14:23:49 +0800

    docs(core): type hinting

---
+++ esi/esi/test/typing_hint_test.py
@@ -0,0 +1,7 @@
+def p(a) :
+    pass
+
+c = p('1')
+d = p('e')
+print(type(c))
+print(type(d))
---
+++ esi/esi/test/word_count.pyi
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+class Count:
+
+    def _collect_words_d(self, word_list):
+        pass
+
+    def _collect_words(self, word_list: list[str]):
+        pass
